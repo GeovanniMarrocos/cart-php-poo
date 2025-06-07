1cbb5cb69a5e983f066ca54d8a789ba8faa37464
GeovanniMarrocos <geovanni.marrocos@gmail.com>
2025-06-07 10:45:11 -0300

    first commit

--- app/Cart.php
+++ app/Cart.php
@@ -4,10 +4,22 @@ class Cart
 {
   public function add(Product $product)
   {
-      if(count($this->getCart()['products']) > 0)
-      {
+      $inCart = false;
+      if(count($this->getCart()['products']) > 0){
+        foreach ($this->getCart()['products'] as $productInCart){
+          if($productInCart->getId() === $product->getId()) {
+            $quantity = $productInCart->getQuantity() + $product->getQuantity();
+            $productInCart->setQuantity($quantity);
+            $inCart = true;
+            break;
+          }
+        }  
+     }
 
-      }
+     if(!$inCart) {
+        var_dump('not in cart');
+     }
+
   } 
 
   public function remove()
